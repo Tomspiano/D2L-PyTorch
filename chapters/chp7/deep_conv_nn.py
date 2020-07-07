c09ba630301ce486445037351ab4020c92344f44
Tomspiano <helloworldbrandnew@163.com>
2020-07-07 23:33:56 +0800

    section 1

---
+++ chapters/chp7/deep_conv_nn.py
@@ -0,0 +1,36 @@
+import os
+import torch
+from torch import nn
+from modules import base
+from modules import fashionMNIST as fmnist
+from modules import d2lCustom as custom
+
+
+def main():
+    batch_size = 128
+
+    lr, eps = .001, 1e-3
+    # lr, eps = .001, 1e-1
+
+    root = '../../Datasets'
+    train_iter, test_iter = fmnist.load_data(batch_size, 224, root)
+
+    net = custom.AlexNet()
+    optimizer = torch.optim.Adam(net.parameters(), lr)
+    ckpt_path = '../../checkpoint/deep_conv_nn.pt'
+    ckpt = None
+    if os.path.exists(ckpt_path):
+        ckpt = torch.load(ckpt_path)
+
+        net.load_state_dict(ckpt['net'])
+        optimizer.load_state_dict(ckpt['optimizer'])
+    loss = nn.CrossEntropyLoss()
+
+    base.train(net, train_iter, test_iter, loss, eps=eps,
+               optimizer=optimizer, checkpoint_path=ckpt_path, checkpoint=ckpt)
+    # epoch 15, loss 0.159, train acc 0.941, test acc 0.916, 1.3 examples/sec
+    # if eps = 1e-3, learning rate = 0.001
+
+
+if __name__ == '__main__':
+    main()
