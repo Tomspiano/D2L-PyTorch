2b4cbc3c6e11fd03f52c507857d1d788cb88610e
Tomspiano <helloworldbrandnew@163.com>
2020-07-11 15:54:18 +0800

    add ResNet

--- modules/d2lCustom.py
+++ modules/d2lCustom.py
@@ -242,6 +242,65 @@ class GoogLeNet(nn.Module):
         return self.net(x)
 
 
+class Residual(nn.Module):
+    def __init__(self, input_channels, num_channels,
+                 use_1x1conv=False, strides=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, num_channels,
+                               kernel_size=3, padding=1, stride=strides)
+        self.conv2 = nn.Conv2d(num_channels, num_channels,
+                               kernel_size=3, padding=1)
+        if use_1x1conv:
+            self.conv3 = nn.Conv2d(input_channels, num_channels,
+                                   kernel_size=1, stride=strides)
+        else:
+            self.conv3 = None
+        self.bn1 = nn.BatchNorm2d(num_channels)
+        self.bn2 = nn.BatchNorm2d(num_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        Y = F.relu(self.bn1(self.conv1(x)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            x = self.conv3(x)
+        Y += x
+        return F.relu(Y)
+
+
+class ResNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.b1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+                                nn.BatchNorm2d(64), nn.ReLU(),
+                                nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+
+        self.b2 = nn.Sequential(*self.resnet_block(64, 64, 2, first_block=True))
+        self.b3 = nn.Sequential(*self.resnet_block(64, 128, 2))
+        self.b4 = nn.Sequential(*self.resnet_block(128, 256, 2))
+        self.b5 = nn.Sequential(*self.resnet_block(256, 512, 2))
+
+        self.net = nn.Sequential(self.b1, self.b2, self.b3, self.b4, self.b5,
+                                 nn.AdaptiveMaxPool2d((1, 1)),
+                                 FlattenLayer(), nn.Linear(512, 10))
+
+    @staticmethod
+    def resnet_block(input_channels, num_channels, num_residuals,
+                     first_block=False):
+        blk = []
+        for i in range(num_residuals):
+            if i == 0 and not first_block:
+                blk.append(Residual(input_channels, num_channels,
+                                    use_1x1conv=True, strides=2))
+            else:
+                blk.append(Residual(num_channels, num_channels))
+        return blk
+
+    def forward(self, x):
+        return self.net(x)
+
+
 def arch(x, layers):
     for layer in layers:
         x = layer(x)
@@ -250,8 +309,8 @@ def arch(x, layers):
 
 
 if __name__ == '__main__':
-    size = (1, 1, 96, 96)
-    nets = [GoogLeNet().net]
+    size = (1, 1, 224, 224)
+    nets = [ResNet().net]
     X = torch.randn(size, dtype=torch.float32)
     for net in nets:
         X = arch(X, net)
